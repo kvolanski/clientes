d5ceb256b1c38a4a581a3041c0d4b1970d05d2ff
Kevin <kvolanski@gmail.com>
2021-02-08 12:11:14 -0300

    Adicionado um builder e um prePersist.

--- src/main/java/io/github/kvolanski/clientes/ClientesApplication.java
+++ src/main/java/io/github/kvolanski/clientes/ClientesApplication.java
@@ -1,11 +1,29 @@
 package io.github.kvolanski.clientes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.kvolanski.clientes.model.entity.Cliente;
+import io.github.kvolanski.clientes.model.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ClientesApplication {
 
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Bean
+	public CommandLineRunner run() {
+		return args ->{
+			Cliente cliente = Cliente.builder().cpf("00000000000").nome("Kevin").build();
+			clienteRepository.save(cliente);
+		};
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
 	}
--- src/main/java/io/github/kvolanski/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/kvolanski/clientes/model/entity/Cliente.java
@@ -8,14 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente implements Serializable {
 	
 	/**
@@ -36,5 +43,9 @@ public class Cliente implements Serializable {
 	@Column(name = "data_cadastro")
 	private LocalDate dataCadastro;
 	
+	@PrePersist
+	public void prePersist() {
+		setDataCadastro(LocalDate.now());
+	}
 	
 }
